7d9deccbcb69a85cde53b76d306ed88c1ed4c1a1
P8USea <funnylov32003@gmail.com>
2024-10-30 21:47:59 +0700

    Modify configs

---
+++ WiFi_Disconnect_QoS0_Publishing/src/secrets/mqtt.h
@@ -0,0 +1,6 @@
+namespace MQTT {
+    const char *broker = "7c37a402b8fa4228953fa73232f36021.s1.eu.hivemq.cloud";
+    const int port = 8883;
+    const char *username = "hoangfuc";
+    const char *password = "29102018";
+}
---
+++ WiFi_Disconnect_QoS0_Publishing/src/secrets/wifi.h
@@ -0,0 +1,4 @@
+namespace WiFiSecrets {
+    const char *ssid = "HoangFuc07";
+    const char *pass = "1highbar45678";
+}
